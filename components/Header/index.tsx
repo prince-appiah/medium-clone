aba9ae9960f3e96394e5e0a23fe2bb0258a48e4c
Prince Appiah <pappiah00@gmail.com>
2022-01-19 18:18:41 +0000

    feat(init): :tada: Begin project

---
+++ components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Header = () => {
+    return <header>
+        
+        <div>
+            <div>
+                <Link href='/' >
+                    <img src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png" alt="" />
+                </Link>
+            </div>
+            <div></div>
+        </div>
+    
+    </header>;
+};
+
+export default Header;
